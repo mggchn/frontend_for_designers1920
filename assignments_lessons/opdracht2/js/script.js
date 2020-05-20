d62112f3569de885434b1ae44f74ad009d8df687
Meggie Chen <meggiechn@gmail.com>
2020-05-20 19:21:55 +0200

    zonder javascript

--- assignments_lessons/opdracht2/js/script.js
+++ assignments_lessons/opdracht2/js/script.js
@@ -3,5 +3,6 @@ const carouselSlider = document.querySelector('.carousel-slider');
 const carouselImages = document.querySelector('.foto');
 
 // buttons aanroepen
-const buttonLeft = document.querySelector('.button-left');
-const buttonRight = document.querySelector('.button-right');
+const buttonLeft = document.querySelector('.button.left');
+const buttonRight = document.querySelector('.button.right');
+
