732f977ae99edecffb89d9e02eaf54270bcf9e05
Meggie Chen <meggiechn@gmail.com>
2020-05-20 14:18:28 +0200

    feedbackgesprek

--- assignments_lessons/opdracht2/js/js.js
+++ assignments_lessons/opdracht2/js/js.js
@@ -1,3 +1,7 @@
+// slider en afbeeldingen aanroepen
 const carouselSlider = document.querySelector('.carousel-slider');
 const carouselImages = document.querySelector('.foto');
 
+// buttons aanroepen
+const buttonLeft = document.querySelector('.button-left');
+const buttonRight = document.querySelector('.button-right');
