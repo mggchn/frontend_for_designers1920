f1f8efc6bc216a3df89966158eceeabba4409a7c
Meggie Chen <meggiechn@gmail.com>
2020-06-17 18:00:49 +0200

    liedjes in slider

--- assignments_lessons/opdracht3/script/script.js
+++ assignments_lessons/opdracht3/script/script.js
@@ -1,5 +1,5 @@
-// Section declareren door deze aan te roepen vanuit de HTML
-const section = document.querySelector('section');
+// Body declareren door deze aan te roepen vanuit de HTML
+const body = document.querySelector('body');
 
 // Laad het JSON bestand in m.b.v. de URL en de regels code (5-14)
 let requestURL = 'https://raw.githubusercontent.com/mggchn/frontend_for_designers1920/master/assignments_lessons/opdracht3/mostlovedsongs.json';
@@ -16,17 +16,25 @@ request.onload = function () {
 function showSongs(jsonObj) {
 	const details = jsonObj.loved;
 	// console.log(details);
+
+	// Elementen voor de slider waar de containers in de <section> komen te zitten
+	const myCarouselContainer = document.createElement('div');
+	myCarouselContainer.classList.add('carousel-container');
+	const myCarouselSlider = document.createElement('ul');
+	myCarouselSlider.classList.add('carousel-slider');
     
     // Als allereerst worden er in deze for loop hieronder allerlei elementen aangemaakt die nodig zijn om de content te tonen. Deze loops worden aangemaakt om door als het ware door elke array te 'loopen'. 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < details.length; i++) {
 		console.log(details[i]);
 
+		// Elementen voor de containers waar de informatie van het lied in komt te staan
 		const myArticle = document.createElement('article');
 		const myAlbumCover = document.createElement('div');
 		myAlbumCover.classList.add('albumcover');
 		const mySongDetails = document.createElement('div');
 		mySongDetails.classList.add('songdetails');
 		
+		// Elementen voor de informatie van het lied
 		const myImg = document.createElement('img');
 		const myPlays = document.createElement('p');
 		myPlays.classList.add('plays');
@@ -35,6 +43,7 @@ function showSongs(jsonObj) {
 		const myAlbumGenre = document.createElement('p');
 		myAlbumGenre.classList.add('albumgenre');
 		
+		// Aanroepen van JSON data door deze te koppelen aan de elementen voor de informatie aan het lied
 		myImg.src = details[i].strTrackThumb;
 		// console.log(myImg.src);
 		myPlays.textContent = details[i].intTotalPlays + " plays";
@@ -46,11 +55,21 @@ function showSongs(jsonObj) {
 		myAlbumGenre.textContent = details[i].strAlbum + " — " + details[i].strGenre;
 		// console.log(myAlbumGenre.textContent);
 
-		// De <article> in de <section> gestopt moet worden
-		section.appendChild(myArticle);
+		// De <div class="carousel-container"> die in de <body> gestopt moet worden
+		body.appendChild(myCarouselContainer);
+
+		// De <div class="carousel-slider"> die in de <div class="carousel-container"> gestopt moet worden
+		myCarouselContainer.appendChild(myCarouselSlider);
+
+		// De <article> die in de <ul class="carousel-slider"> gestopt moet worden
+		myCarouselSlider.appendChild(myArticle);
+
 		// De <div> die in de <article> gestopt moeten worden
 		myArticle.appendChild(myAlbumCover);
 		myArticle.appendChild(mySongDetails);
+
+		// De album cover die in de <div class="albumcover" gestopt moet
+		myAlbumCover.appendChild(myImg);
 
 		// De details van het lied die in de <div class="songdetails"> gestopt moet worden
 		mySongDetails.appendChild(myPlays);
@@ -58,8 +77,5 @@ function showSongs(jsonObj) {
 		mySongDetails.appendChild(mySongTitle);
 		mySongDetails.appendChild(myArtist);
 		mySongDetails.appendChild(myAlbumGenre);
-
-		// De album cover die in de <div class="albumcover" gestopt moet
-		myAlbumCover.appendChild(myImg);
 	}
 }
