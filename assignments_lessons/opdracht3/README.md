04ab2d047de8b1e3598614bb0814381510faf9ed
Meggie Chen <meggiechn@gmail.com>
2020-06-18 15:41:55 +0200

    Laatste versie: werkend

--- assignments_lessons/opdracht3/README.md
+++ assignments_lessons/opdracht3/README.md
@@ -1,23 +1,33 @@
 # Frontend voor Designers - opdracht 3: Een interactie uitwerken met externe data
 
-Voor deze opdracht ga je een functionaliteit ontwerpen met externe data. De data is JSON die met een [REST API](https://developer.mozilla.org/en-US/docs/Glossary/REST) van een externe bron wordt ingeladen met Javascript.  Als de data geladen is moeten gebruikers je ontwerp op verschillende manieren kunnen bedienen. Verschillende states zijn vormgeven en worden op het juiste moment getoond.
+Voor deze opdracht ga ik een functionaliteit ontwerpen met externe data. De data is JSON die met een [REST API](https://developer.mozilla.org/en-US/docs/Glossary/REST) van een externe bron wordt ingeladen met Javascript.  Als de data geladen is moeten gebruikers mijn ontwerp op verschillende manieren kunnen bedienen. Verschillende states zijn vormgeven en worden op het juiste moment getoond.
 
 Lees hier de [opdrachtbeschrijving](./opdrachtbeschrijving.md).
 
 
-# Project titel
-Beschrijf hier je eigen project.
-En voeg een link naar je demo toe.
+# Top 50 Most Loved Songs
+Voor dit project heb ik een pagina gecodeerd waarop de Top 50 Most Loved Songs te bekijken zijn in een vorm van een carousel.
+De carousel is navigeerbaar met behulp van de links/rechts knoppen onder de liedjes en ook via de links/rechts – W/D knoppen op het toetsenbord.
 
-## interface
-Leg de interface uit.
-
-In de demo heb je interface design principles 04, 08, 09 & 11 van [Principles of User Interface Design](http://bokardo.com/principles-of-user-interface-design/) toegepast. Hoe heb je dat gedaan?
-
-In de demo heb je meerdere [UI events](https://developer.mozilla.org/en-US/docs/Web/API/UIEvent) toegepast. Hoe heb je dat gedaan?
-
-IN de demo heb je een aantal states van de [UI stack](https://www.scotthurff.com/posts/why-your-user-interface-is-awkward-youre-ignoring-the-ui-stack/) toegepast. Hoe heb je dat gedaan?
+De data die ik voor dit project heb gebruikt, is te vinden in [dit JSON file](https://www.theaudiodb.com/api/v1/json/5d656564694f534d656564/mostloved.php?format=track&format=track).
 
 
-## code
-Leg de code uit.
+## Principles of User Interface Design
+In mijn ontwerp heb ik interface design principles 04, 08, 09 & 11 van [Principles of User Interface Design](http://bokardo.com/principles-of-user-interface-design/) toegepast. Deze zijn hieronder toegelicht:
+
+### 04. Keep users in control
+Dit principle is binnen mijn ontwerp terug te vinden bij de twee buttons, met elk een pijl op de button aangegeven die naar links of rechts wijst. Deze geven duidelijk aan wat er zal gaan gebeuren als er op de button gedrukt wordt, namelijk het naar links of rechts navigeren.
+
+### 08. Provide a natural next step
+Dit principle is binnen mijn ontwerp te vinden bij de carousel. De liedjes aan de buitenkant worden niet volledig weergegeven in de slider, wat betekent dat er nog iets achter zit. De gebruiker zal dit ook verwachten en zal dus m.b.v. de buttons of de toetsen navigeren, zodat de liedjes volledig in beeld te zien zijn.
+Op de buttons zijn er een hoverstate aanwezig. Dit laat een sein bij de gebruiker achter dat deze buttons klikbaar zijn.
+
+### 09. Appearance follows behavior
+Dit principle is binnen mijn ontwerp verwerkt door de buttons er ook klikbaar uit te laten zien. Dit is toegepast door een schaduw toe te voegen en een randje toe te voegen aan de knop om het als een blokje uit te laten zien.
+
+### 11. Strong visual hierarchies work best
+De visuele hiërarchie is binnen mijn ontwerp terug te zien binnen de teksten op de pagina. De headings zijn groter dan de broodtekst en dikgedrukt. Voor de broodtekst is er in het algemeen sprake van een reguliere font-weight, maar voor sommige stukken tekst is dit een uitzondering. Voor het x aantal plays dat wordt laten zien bij elk lied is deze bijvoorbeeld grijs gemaakt en is deze in hoofdletters weergegeven, zodat er extra contrast met de rest van de tekst zit. 
+
+## States
+
+## Code
--- assignments_lessons/opdracht3/script/script.js
+++ assignments_lessons/opdracht3/script/script.js
@@ -2,6 +2,9 @@
 
 // Elementen declareren door deze aan te roepen vanuit de HTML
 const body = document.querySelector('body');
+const previousButton = document.querySelector('#previousButton');
+const nextButton = document.querySelector('#nextButton');
+var details;
 
 // Laad het JSON bestand in m.b.v. de URL en de regels code (5-14)
 let requestURL = 'https://raw.githubusercontent.com/mggchn/frontend_for_designers1920/master/assignments_lessons/opdracht3/mostlovedsongs.json';
@@ -10,45 +13,41 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function () {
-    const songs = request.response;
+	const songs = request.response;
 	// console.log(songs);
 	showSongs(songs);
+	showOther();
 }
 
 function showSongs(jsonObj) {
-	const details = jsonObj.loved;
+	var details = jsonObj.loved;
 	// console.log(details);
 
 	// Elementen voor de slider waar de containers in de <section> komen te zitten
 	const myCarouselContainer = document.createElement('div');
 	myCarouselContainer.classList.add('carousel-container');
-	const myCarouselSlider = document.createElement('ul');
+	let myCarouselSlider = document.createElement('ul');
 	myCarouselSlider.classList.add('carousel-slider');
+	let allSongs = document.createElement('div');
+	allSongs.classList.add('allsongs');
 
-	///// CONTAINER EN DE BIJBEHORENDE BUTTONS AANMAKEN /////
 	// Elementen voor de container van de buttons
 	const myButtonContainer = document.createElement('div');
 	myButtonContainer.classList.add('button-container');
 
-	// Elementen voor de buttons
-	const previousButton = document.createElement('button');
-	previousButton.classList.add('previous-button');
-	previousButton.innerHTML = '&#8249';
-	const nextButton = document.createElement('button');
-	nextButton.classList.add('next-button');
-	nextButton.innerHTML = '&#8250';
-
-    // Als allereerst worden er in deze for loop hieronder allerlei elementen aangemaakt die nodig zijn om de content te tonen. Deze loops worden aangemaakt om door als het ware door elke array te 'loopen'. 
-    for (let i = 0; i < details.length; i++) {
+	// Als allereerst worden er in deze for loop hieronder allerlei elementen aangemaakt die nodig zijn om de content te tonen. 
+	// Deze loops worden aangemaakt om door als het ware door elke array te 'loopen'. 
+	for (let i = 0; i < details.length; i++) {
 		// console.log(details[i]);
 
 		// Elementen voor de containers waar de informatie van het lied in komt te staan
 		const myArticle = document.createElement('article');
+		myArticle.classList.add('articlesongs');
 		const myAlbumCover = document.createElement('div');
 		myAlbumCover.classList.add('albumcover');
 		const mySongDetails = document.createElement('div');
 		mySongDetails.classList.add('songdetails');
-		
+
 		// Elementen voor de informatie van het lied
 		const myImg = document.createElement('img');
 		const myPlays = document.createElement('p');
@@ -57,7 +56,7 @@ function showSongs(jsonObj) {
 		const myArtist = document.createElement('p');
 		const myAlbumGenre = document.createElement('p');
 		myAlbumGenre.classList.add('albumgenre');
-		
+
 		// Aanroepen van JSON data door deze te koppelen aan de elementen voor de informatie aan het lied
 		myImg.src = details[i].strTrackThumb;
 		myPlays.textContent = details[i].intTotalPlays + " plays";
@@ -69,19 +68,23 @@ function showSongs(jsonObj) {
 		body.appendChild(myCarouselContainer);
 		body.appendChild(myButtonContainer);
 
+
 		// De <div class="carousel-slider"> die in de <div class="carousel-container"> gestopt moet worden
 		myCarouselContainer.appendChild(myCarouselSlider);
-		
+
 		// De <article> die in de <ul class="carousel-slider"> gestopt moet worden
 		myCarouselSlider.appendChild(myArticle);
-		
+		myCarouselSlider.appendChild(allSongs);
+
+		allSongs.appendChild(myArticle);
+
 		// De <div> die in de <article> gestopt moeten worden
 		myArticle.appendChild(myAlbumCover);
 		myArticle.appendChild(mySongDetails);
-		
+
 		// De album cover die in de <div class="albumcover" gestopt moet worden
 		myAlbumCover.appendChild(myImg);
-		
+
 		// De details van het lied die in de <div class="songdetails"> gestopt moet worden
 		mySongDetails.appendChild(myPlays);
 		mySongDetails.appendChild(mySongTitle);
@@ -92,18 +95,49 @@ function showSongs(jsonObj) {
 		//  De <button> die in de <div class="button-container"> gestopt moet worden
 		myButtonContainer.appendChild(previousButton);
 		myButtonContainer.appendChild(nextButton);
+
+		mySongs = document.querySelector('article');
+		// console.log(mySongs);
 	}
+
+	divSongs = document.querySelector('.allsongs');
+	// console.log(divSongs);
 }
 
-///// DE CAROUSEL CLICKABLE KRIJGEN /////
 
-const songs = document.querySelector('article');
-const previousButton = document.querySelector('.previous-button');
-const nextButton = document.querySelector('next-button');
-console.log(nextButton);
-nextButton.addEventListener('click', nextSong());
+///// HET BEWEGEN VAN DE SLIDER /////
 
-function nextSong() {
-	currentSong = (currentSong < 11) ? currentSong + 1 : 11;
-    songs.style.transform = 'translate(' + (currentSong) * -25 + '%)';
+// Hiermee geef ik de huidige plek een positie mee van 0, voordat hij naar links/rechts beweegt. 
+var position = 0;
+
+// Bron: https://stackoverflow.com/questions/54169955/trying-to-apply-css-transform-in-javascript
+// Als de slider naar links gaat, is de positie -1. Hij zal dan -50% naar links verschuiven.
+previousButton.onclick = function () {
+	position = (position > 0) ? position - 1 : 0;
+	// console.log(position);
+	divSongs.style.transform = 'translate(calc(' + (position) * -50 + '%)';
+	// console.log(divSongs.style.transform);
+}
+
+// Als de slider naar rechts gaat, is de positie +1. Hij zal dan 50% naar rechts verschuiven.
+// Ik geef hier een positie mee van 40. Dit betekent dat hij stopt bij de 40e klik, oftewel: einde van de carousel.
+nextButton.onclick = function () {
+	position = (position < 40) ? position + 1 : 40;
+	// console.log(position);
+	divSongs.style.transform = 'translate(calc(' + (position) * -50 + '%)';
+	// console.log(divSongs.style.transform);
+}
+
+// De slider laten bewegen met de pijltjes van het toetsenbord.
+document.addEventListener('keydown', controls);
+
+// Met 37 of 'W' (pijltje links) ga je naar links.
+// Met 39 of 'D' (pijltje rechts) ga je naar rechts.
+function controls(e) {
+	// console.log(e.which);
+	if (e.which === 37 || e.which === 65) {
+		previousButton.onclick();
+	} else if (e.which === 39 || e.which === 68) {
+		nextButton.onclick();
+	}
 }
